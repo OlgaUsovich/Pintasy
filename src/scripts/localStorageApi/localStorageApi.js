be3b795e840d57f575958b20cf56ba5c063a754b
Mazunok <mazun.1999@icloud.com>
2022-06-08 20:13:14 +0300

    refactor(local-storage-1):create local storage functions

---
+++ src/scripts/localStorageApi/localStorageApi.js
@@ -0,0 +1,10 @@
+const getStorageData = (key ) =>{
+    const storageData = JSON.parse(localStorage.getItem(key));
+
+    return storageData;
+};
+
+const setStorageData = (key, value) =>{
+    localStorage.setItem(key, JSON.stringify(value));
+};
+
