c43b9b51e6e6e1b1c40797fcf97252d12ec7ffb6
Mazunok <mazun.1999@icloud.com>
2022-06-09 23:26:50 +0300

    refactor(fake-get-data):delete openDrop from import in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import { openDrop, addDropdownListener } from "./components/header.js";
+import { addDropdownListener } from "./components/header.js";
 import cardsJson from "../data/cards.json";
 import boardsJson from "../data/boards.json";
 import { getStorageData, setStorageData } from "./localStorageApi/localStorageApi.js";
