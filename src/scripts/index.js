b451d8bab3ca87b92d13eb99d9f323ec3c2c2fa0
Mazunok <mazun.1999@icloud.com>
2022-06-09 22:56:21 +0300

    refactor(fake-get-data):add check for local storage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,18 @@
 import { openDrop, addDropdownListener } from "./components/header.js";
-import cards from "../data/cards.json";
-import boards from "../data/boards.json";
-import {getStorageData, setStorageData} from "./localStorageApi/localStorageApi.js"
+import cardsJson from "../data/cards.json";
+import boardsJson from "../data/boards.json";
+import { getStorageData, setStorageData } from "./localStorageApi/localStorageApi.js";
 
 init()
 
 function init () {
     addDropdownListener()
+
+    if(!getStorageData('cards')){
+        setStorageData('cards', cardsJson);
+    }
+
+    if(!getStorageData('boards')){
+        setStorageData('boards', boardsJson)
+    }
 }
