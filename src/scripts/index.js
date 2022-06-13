e483ddbcc352470a05718d7a931082ad4f1fb7a8
Alex Kurov <Alex>
2022-06-13 17:09:53 +0300

    feat(get-data-from-server): add a new function for getting and rendering data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import { addDropdownListener } from "./components/header.js";
 import cardsJson from "../data/cards.json";
 import boardsJson from "../data/boards.json";
 import { renderBoards } from "./components/boards.js";
+import { getMockData, url } from "./mockApi/mockApi.js";
 import { getStorageData, setStorageData} from "./localStorageApi/localStorageApi.js";
 
 init();
@@ -18,4 +19,5 @@ function init() {
   }
   const boards = getStorageData("boards");
   renderBoards(boards);
+  getMockData(url, renderBoards)
 }
---
+++ src/scripts/mockApi/mockApi.js
@@ -0,0 +1,11 @@
+import {URL_BOARDS} from "./constantLinks.js"
+
+
+function getMockData (link, renderFunction) {
+    fetch (link)
+.then(res => res.json())
+.then(renderFunction)
+}
+
+getMockData(URL_BOARDS)
+export {getMockData, URL_BOARDS}
