6c8a94914d6fb373e1d8e1a869d8d6e1793a53ab
Сергей <99472918+Stalexus@users.noreply.github.com>
2022-06-16 21:33:36 +0300

    refactor(crate-card-1): delete text in createElement img and added destructuring

--- src/scripts/components/cards.js
+++ src/scripts/components/cards.js
@@ -1,18 +1,18 @@
 import { createElement } from "../utils/helpers/helpers.js";
-import { getStorageData } from "../localStorageApi/localStorageApi.js"
+import { getStorageData } from "../localStorageApi/localStorageApi.js";
 
-function createCard(cardData) {
+function createCard( {cardData} ) {
     const card = createElement('div', 'm-2 card-item');
     const imageContainer = createElement('div', 'position-relative');
-    const image = createElement('img', 'card-image rounded-4', cardData.image)
+    const image = createElement('img', 'card-image rounded-4');
     const menuBtn = createElement('button', 'modal-menu rounded-circle position-absolute bg-aqua bg-opacity-75');
     const descriptionContainer = createElement('div', 'd-flex gap-3 mt-2');
-    const avatar = createElement('img', 'card-avatar rounded-circle', cardData.avatar)
-    const description = createElement('span', 'card-description', cardData.description)
+    const avatar = createElement('img', 'card-avatar rounded-circle');
+    const description = createElement('span', 'card-description');
 
-    image.src = cardData.image;
-    avatar.src = cardData.avatar;
-    description = document.createTextNode(cardData.description);
+    image.src = image;
+    avatar.src = avatar;
+    description = document.createTextNode(description);
 
     imageContainer.append(image, menuBtn);
     descriptionContainer.append(avatar, description);
