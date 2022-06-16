10a6c400312033361ac1bdb32c883264cd878101
Alex Kurov <Alex>
2022-06-16 23:55:57 +0300

    fix(pin-1): all mistakes have been fixed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,4 @@
 import { addDropdownListener } from "./components/Header.js";
-import cardsJson from "../data/cards.json";
-import boardsJson from "../data/boards.json";
 import { renderBoards } from "./components/Boards.js";
 import { setTestData, getStorageData } from "./localStorageApi/localStorageApi.js";
 
