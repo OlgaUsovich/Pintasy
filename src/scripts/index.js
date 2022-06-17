f1a62b59eba6d517cae3f283b2caf26caad3b43a
Mazunok <mazun.1999@icloud.com>
2022-06-17 17:45:58 +0300

    refactor(add-card-to-board):delete imports from index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,4 @@
 import { addDropdownListener } from "./components/header.js";
-import cards from "../data/cards.json";
-import board from "../data/boards.json";
-import { renderBoards } from "./components/boards.js";
-import {
-  getStorageData,
-  setStorageData,
-} from "./localStorageApi/localStorageApi.js";
-import { addDropdownListener } from "./components/Header.js";
-import { renderBoards } from "./components/Boards.js";
-import { setTestData, getStorageData } from "./localStorageApi/localStorageApi.js";
 import { renderBoards } from "./components/boards.js";
 import { renderCards } from "./components/Сards.js";
 import { getStorageData, setStorageData, setTestData } from "./localStorageApi/localStorageApi.js";
