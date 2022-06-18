6b5f3bb8e58b0ab343a1808ee647aa5521374735
Alex Kurov <Alex>
2022-06-18 22:01:13 +0300

    refactor(choose-board): rename Header.js file

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -1,13 +1,14 @@
 const openDrop = ({ target }) => {
-  const bordsMenuItem = document.querySelector("#dropdown-list");
-  if (target.id === "dropdown-menu-btn") {
-    bordsMenuItem.classList.toggle("d-block");
-  }
-};
-
-const addDropdownListener = () => {
-  const header = document.querySelector("#header");
-  header.addEventListener("click", openDrop);
-};
-
-export { addDropdownListener };
+    const bordsMenuItem = document.querySelector("#dropdown-list");
+    if (target.id === "dropdown-menu-btn") {
+      bordsMenuItem.classList.toggle("d-block");
+    }
+  };
+  
+  const addDropdownListener = () => {
+    const header = document.querySelector("#header");
+    header.addEventListener("click", openDrop);
+  };
+  
+  export { addDropdownListener };
+  
