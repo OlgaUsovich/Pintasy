13ef79d7736c18e2f3c9c944ec989bcb5b3795b1
Alex Kurov <Alex>
2022-06-18 17:49:40 +0300

    refactor(choose-board): removed unnecessary imports

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,7 @@
 import { addDropdownListener } from "./components/header.js";
-import { renderBoards, addClickBoardItem } from "./components/boards.js";
+import { renderBoards } from "./components/boards.js";
 import { renderCards } from "./components/Сards.js";
-import {
-  getStorageData,
-  setStorageData,
-  setTestData,
-} from "./localStorageApi/localStorageApi.js";
+import { getStorageData,setTestData } from "./localStorageApi/localStorageApi.js";
 
 init();
 
