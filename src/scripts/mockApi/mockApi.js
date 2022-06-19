2d3a3556053e4d4f18848471d1f09fdb087be1ec
Alex Kurov <Alex>
2022-06-19 18:05:45 +0300

    refactor(pin-2): formatted mockApi.js

--- src/scripts/mockApi/mockApi.js
+++ src/scripts/mockApi/mockApi.js
@@ -2,10 +2,10 @@ const getCards = (url) =>
   new Promise((resolve, reject) =>
     fetch(url)
       .then((response) => {
-        if(response.status === 404) {
-          console.log('error 404!!!!') 
-        }else{
-          return response.json()
+        if (response.status === 404) {
+          console.log("error 404!!!!");
+        } else {
+          return response.json();
         }
       })
       .then((data) => resolve(data))
