b695e5f43fcb9f058c0702822820090d86ab6828
Alex Kurov <Alex>
2022-06-20 23:36:41 +0300

    feat(pin-11): added functions to open and close modal window

--- src/scripts/modalWindow/modalWindow.js
+++ src/scripts/modalWindow/modalWindow.js
@@ -1,3 +1,12 @@
 import { createElement } from "../utils/helpers/helpers";
 
+function closeModalWindow({target}) {
+    if(target.id === 'btn-close') {
+        const modalWindow = document.querySelector('#modal')
+        modalWindow.style.display = 'none'  
+    }
+    
+}
+
+export {closeModalWindow}
 
