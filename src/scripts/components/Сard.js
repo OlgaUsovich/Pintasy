254eb54815078cc8d5dc11fde7d51e2f5cde4fea
Olga <olga@gmail.com>
2022-06-20 22:36:50 +0300

    refactor(pin-17): use destructuring assignment in create card function

--- src/scripts/components/Сard.js
+++ src/scripts/components/Сard.js
@@ -4,22 +4,22 @@ import { createElement } from "../utils/helpers/helpers.js";
 import { getStorageData, setStorageData } from "../localStorageApi/localStorageApi.js";
 import { BOARDS, REPORTED } from "../localStorageApi/constants.js";
 
-function createCard(cardData) {
+function createCard({id: cardId, image: cardImage, description: cardDescription, avatar: cardAvatar}) {
     const card = createElement('div', 'p-2 card-item');
     const imageContainer = createElement('div', 'position-relative');
     const image = createElement('img', 'card-image rounded-4');
     const menuBtn = createElement('button', 'modal-menu rounded-circle position-absolute bg-aqua bg-opacity-75');
     const descriptionContainer = createElement('div', 'd-flex gap-3 mt-2');
     const avatar = createElement('img', 'card-avatar rounded-circle');
-    const description = createElement('span', 'card-description', cardData.description);
+    const description = createElement('span', 'card-description', cardDescription);
     const cardMenu = createElement('div', 'card-menu d-none bg-secondary position-absolute rounded-4 p-3 bg-opacity-75 d-flex flex-column justify-content-center align-items-stretch gap-3');
     const addBtn = createElement('button', 'btn btn-outline-dark','Add to board');
     const complaintBtn = createElement('button', 'btn btn-outline-dark','Complain');
 
-    card.id = cardData.id;
-    menuBtn.id = cardData.id;
-    image.src = cardData.image;
-    avatar.src = cardData.avatar;
+    card.id = cardId;
+    menuBtn.id = cardId;
+    image.src = cardImage;
+    avatar.src = cardAvatar;
 
     cardMenu.append(addBtn, complaintBtn);
     imageContainer.append(image, menuBtn, cardMenu);
