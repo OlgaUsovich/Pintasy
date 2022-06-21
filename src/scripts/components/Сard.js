01e87c02ecf71fbf7a7542017560ec08cbb97d30
Olga <olga@gmail.com>
2022-06-21 23:04:55 +0300

    feat(pin-18): add feature of adding a card to a board

--- src/scripts/components/Сard.js
+++ src/scripts/components/Сard.js
@@ -1,48 +1,49 @@
 import Masonry from "masonry-layout";
 
-import { createElement } from "../utils/helpers/helpers.js";
-import { getStorageData, setStorageData } from "../localStorageApi/localStorageApi.js";
-import { BOARDS, REPORTED } from "../localStorageApi/constants.js";
-import { openModalWindow, closeModalWindow }  from "../modal/modalWindow.js"
+import {createElement} from "../utils/helpers/helpers.js";
+import {getStorageData, setStorageData} from "../localStorageApi/localStorageApi.js";
+import {BOARDS, REPORTED} from "../localStorageApi/constants.js";
+import {openModalWindow, closeModalWindow} from "../modal/modalWindow.js"
 
 function createCard({id: cardId, image: cardImage, description: cardDescription, avatar: cardAvatar}) {
-  const card = createElement('div', 'p-2 card-item');
-  const imageContainer = createElement('div', 'position-relative');
-  const image = createElement('img', 'card-image rounded-4');
-  const menuBtn = createElement('button', 'modal-menu rounded-circle position-absolute bg-aqua bg-opacity-75');
-  const descriptionContainer = createElement('div', 'd-flex gap-3 mt-2');
-  const avatar = createElement('img', 'card-avatar rounded-circle');
-  const description = createElement('span', 'card-description', cardDescription);
-  const cardMenu = createElement('div', 'card-menu d-none bg-secondary position-absolute rounded-4 p-3 bg-opacity-75 d-flex flex-column justify-content-center align-items-stretch gap-3');
-  const addBtn = createElement('button', 'btn btn-outline-dark','Add to board');
-  const complaintBtn = createElement('button', 'btn btn-outline-dark','Complain');
+    const card = createElement('div', 'p-2 card-item');
+    const imageContainer = createElement('div', 'position-relative');
+    const image = createElement('img', 'card-image rounded-4');
+    const menuBtn = createElement('button', 'modal-menu rounded-circle position-absolute bg-aqua bg-opacity-75');
+    const descriptionContainer = createElement('div', 'd-flex gap-3 mt-2');
+    const avatar = createElement('img', 'card-avatar rounded-circle');
+    const description = createElement('span', 'card-description', cardDescription);
+    const cardMenu = createElement('div', 'card-menu d-none bg-secondary position-absolute rounded-4 p-3 bg-opacity-75 d-flex flex-column justify-content-center align-items-stretch gap-3');
+    const addBtn = createElement('button', 'btn btn-outline-dark', 'Add to board');
+    const complaintBtn = createElement('button', 'btn btn-outline-dark', 'Complain');
 
-  card.id = cardId;
-  menuBtn.id = cardId;
-  image.src = cardImage;
-  avatar.src = cardAvatar;
-  addBtn.id = "add-button";
+    card.id = cardId;
+    menuBtn.id = cardId;
+    image.src = cardImage;
+    avatar.src = cardAvatar;
+    addBtn.id = "add-button";
+    addBtn.dataset.card = cardId;
 
-  cardMenu.append(addBtn, complaintBtn);
-  imageContainer.append(image, menuBtn, cardMenu);
-  descriptionContainer.append(avatar, description);
-  card.append(imageContainer, descriptionContainer);
+    cardMenu.append(addBtn, complaintBtn);
+    imageContainer.append(image, menuBtn, cardMenu);
+    descriptionContainer.append(avatar, description);
+    card.append(imageContainer, descriptionContainer);
 
-  const modalWindow = document.querySelector("#modal");
+    const modalWindow = document.querySelector("#modal");
 
-  menuBtn.addEventListener("click", onCardMenuClick);
+    menuBtn.addEventListener("click", onCardMenuClick);
 
-  addBtn.addEventListener("click", openModalWindow);
-  modalWindow.addEventListener("click", closeModalWindow);
+    addBtn.addEventListener("click", openModalWindow);
+    modalWindow.addEventListener("click", closeModalWindow);
 
-  return card;
+    return card;
 }
 
-function onCardMenuClick({ target }) {
-  if (target.id === this.id) {
-    const cardMenu = this.nextSibling;
-    cardMenu.classList.toggle("d-none");
-  }
+function onCardMenuClick({target}) {
+    if (target.id === this.id) {
+        const cardMenu = this.nextSibling;
+        cardMenu.classList.toggle("d-none");
+    }
 }
 
 function renderCards(cards) {
@@ -74,15 +75,15 @@ function renderCards(cards) {
 }
 
 function addCardToBoard(cardId, boardId) {
-  const boards = getStorageData(BOARDS);
+    const boards = getStorageData(BOARDS);
 
-  boards.forEach((board) => {
-    if (board.id === boardId) {
-      board.cardsIds.push(cardId);
-    }
-  });
+    boards.forEach((board) => {
+        if (board.id === boardId) {
+            board.cardsIds.push(cardId);
+        }
+    });
 
-  setStorageData(BOARDS, boards);
+    setStorageData(BOARDS, boards);
 }
 
 function complainCard(cardId) {
@@ -96,4 +97,4 @@ function complainCard(cardId) {
     setStorageData(REPORTED, reportedCards);
 }
 
-export { createCard, renderCards, addCardToBoard, complainCard }
+export {createCard, renderCards, addCardToBoard, complainCard}
--- src/scripts/utils/helpers/helpers.js
+++ src/scripts/utils/helpers/helpers.js
@@ -10,4 +10,19 @@ const capitalize = (str) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
-export { createElement, capitalize };
+const showMessage = (text, bobyStyle='', btnStyle='') => {
+  const toastMessage = document.getElementById('toast-massage');
+  const messageBody = document.getElementById('toast-message-text');
+  const btn = document.getElementById('toast-massage-btn');
+
+  toastMessage.classList.remove('text-bg-success', 'text-bg-danger');
+  toastMessage.classList.add(bobyStyle);
+  btn.classList.add(btnStyle);
+  messageBody.textContent = text;
+
+  const toast = new bootstrap.Toast(toastMessage)
+
+  toast.show()
+}
+
+export { createElement, capitalize, showMessage };
